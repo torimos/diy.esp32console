4e003311b9a8ecdaea200c976c961fd3a81fed4f
Oleg Lytvynenko (Personal) <oleg.v.lytvynenko@gmail.com>
2021-05-16 17:14:28 +0300

    latest

--- src/main.cpp
+++ src/main.cpp
@@ -12,74 +12,50 @@
 #define TFT_DC   26//2 //black
 #define TFT_RST  33//15 //blue
 
+#define RIGHT_BTNS 36
+#define LEFT_VX 4
+#define LEFT_VY 0
+
+#define UP_BTN 1
+#define LEFT_BTN 1<<4
+#define DOWN_BTN 1<<3
+#define RIGHT_BTN 1<<2
+#define CENTER_BTN 1<<5
+#define JOY_BTN 1<<6
+
 SPIClass tftSPI(VSPI);
-//Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_MOSI, TFT_CLK, TFT_RST, TFT_MISO);
 Adafruit_ILI9341 tft = Adafruit_ILI9341(&tftSPI, TFT_DC, TFT_CS, TFT_RST);
 
-void backlighting(bool state);
-unsigned long testText();
-unsigned long testFillScreen();
+void calibrate();
+
+float mapfloat(float x, float in_min, float in_max, float out_min, float out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("ILI9341 Test!"); 
 
   tft.begin();
-}
-
-void loop(void) {
-  Serial.println(testFillScreen());
-  delay(500);
-
-  for(uint8_t rotation=0; rotation<4; rotation++) {
-    Serial.print(F("Text (rotation="));
-    Serial.print(rotation);
-    Serial.print(F(")        "));
-    tft.setRotation(rotation);
-    Serial.println(testText());
-    delay(1000);
-  }
-  delay(3000);
-}
-
-unsigned long testFillScreen() {
-  unsigned long start = micros();
+  tft.setRotation(1);
   tft.fillScreen(ILI9341_BLACK);
-  yield();
-  tft.fillScreen(ILI9341_RED);
-  yield();
-  tft.fillScreen(ILI9341_GREEN);
-  yield();
-  tft.fillScreen(ILI9341_BLUE);
-  yield();
-  tft.fillScreen(ILI9341_BLACK);
-  yield();
-  return micros() - start;
-}
-
-unsigned long testText() {
-  tft.fillScreen(ILI9341_BLACK);
-  unsigned long start = micros();
   tft.setCursor(0, 0);
-  tft.setTextColor(ILI9341_WHITE);  tft.setTextSize(1);
-  tft.println("Hello World!");
-  tft.setTextColor(ILI9341_YELLOW); tft.setTextSize(2);
-  tft.println(1234.56);
-  tft.setTextColor(ILI9341_RED);    tft.setTextSize(3);
-  tft.println(0xDEADBEEF, HEX);
-  tft.println();
-  tft.setTextColor(ILI9341_GREEN);
-  tft.setTextSize(5);
-  tft.println("Groop");
-  tft.setTextSize(2);
-  tft.println("I implore thee,");
+  tft.setTextColor(ILI9341_YELLOW); 
   tft.setTextSize(1);
-  tft.println("my foonting turlingdromes.");
-  tft.println("And hooptiously drangle me");
-  tft.println("with crinkly bindlewurdles,");
-  tft.println("Or I will rend thee");
-  tft.println("in the gobberwarts");
-  tft.println("with my blurglecruncheon,");
-  tft.println("see if I don't!");
-  return micros() - start;
+
 }
+void loop(void) {
+  tft.fillScreen(ILI9341_BLACK);
+  tft.setCursor(0,0);
+  long val_X = (analogRead(LEFT_VX) - 1880)/6;
+  long val_Y = 215 - (analogRead(LEFT_VY) - 2940);
+  tft.print(val_X); 
+  tft.print(" : ");
+  tft.println(val_Y);
+  tft.drawLine(0, 120-val_Y/1.5, 319, 120-val_Y/1.5, ILI9341_GREEN);
+  tft.drawLine(160-val_X/1.2, 0, 160-val_X/1.2, 239, ILI9341_GREEN);
+  delay(50);
+}
+
+// X:  330 / 313 / 294     -17 | +19
+// Y:   87 / 188 / 293       -101  | +105
